36457597260906ad0ecf37fddfd5d5a5a44e59eb
Ivan Dunskiy <i.dunskiy@gmail.com>
2015-10-17 18:17:30 +0300

    News model creation

---
+++ app/src/main/java/mobapply/newzgamification/model/NewsItem.java
@@ -0,0 +1,46 @@
+package mobapply.newzgamification.model;
+
+import java.util.Date;
+
+/**
+ * Created by ${IvanDunskiy} on 10/17/15.
+ * Copyright (c) 2015 ${MobApply}. All rights reserved.
+ */
+public class NewsItem {
+    Date date;
+    String summary;
+    String textFull;
+    String image;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getTextFull() {
+        return textFull;
+    }
+
+    public void setTextFull(String textFull) {
+        this.textFull = textFull;
+    }
+}
